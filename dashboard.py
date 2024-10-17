e7b51afb38ca2339c0e386a6a93f64beeeb371d9
Henrik Pedersen <Henrik.Pedersen.vs@gmail.com>
2024-10-17 14:17:00 +0200

    Graph för selectbox på video,date,views

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,12 @@
 import streamlit as st 
 from frontend.kpi import ContentKPI
-from frontend.graphs import ViewsTrend
+from frontend.graphs import ViewsTrend, VideoViewsTrend
 
 
 # device_kpi = DeviceKPI()
 content_kpi = ContentKPI()
 views_graph = ViewsTrend()
+video_views_graph = VideoViewsTrend()
 
 def layout():
     st.markdown("# The data driven youtuber")
@@ -14,6 +15,9 @@ def layout():
     # device_kpi.display_device_summary()
     content_kpi.display_content()
     views_graph.display_plot()
+    
+    #La till en selectbox för att kunna se hur traffiken går per video
+    video_views_graph.display_plot_selectbox()
 
 if __name__ == "__main__":
     layout()
--- frontend/graphs.py
+++ frontend/graphs.py
@@ -7,10 +7,35 @@ class ViewsTrend:
         self.df = QueryDatabase("SELECT * FROM marts.views_per_date").df
         print(self.df)
 
+
     def display_plot(self):
         fig = px.line(self.df, x="Datum", y="Visningar")
         st.markdown("## Antal visningar under senaste månaden")
         st.plotly_chart(fig)
 
-# create more graphs here
+    def display_plot_selectbox(self):
+        # Use 'Datum' column for the selectbox since 'Videotitel' doesn't exist
+        video_title = st.selectbox("Select Date:", self.df["Datum"].unique())
+        # Filter the DataFrame based on the selected date
+        filtered_df = self.df[self.df["Datum"] == video_title]
+
+        fig = px.line(filtered_df, x="Datum", y="Visningar")
+        st.plotly_chart(fig)
+        
+        
+
+class VideoViewsTrend:              #Ny selecbox
+    def __init__(self) -> None:
+        self.df = QueryDatabase("""
+            SELECT "Videotitel", "Datum", "Visningar"
+            FROM innehall.diagramdata""").df
+        print(self.df.columns)
+
+    def display_plot_selectbox(self):
+        video_title = st.selectbox(
+            "Select Video Title:", self.df["Videotitel"].unique())
+        filtered_df = self.df[self.df["Videotitel"] == video_title]
+        fig = px.line(filtered_df, x="Datum", y="Visningar",
+                      title=f"Views for {video_title}")
+        st.plotly_chart(fig)
 
